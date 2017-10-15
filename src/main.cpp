66dd8ee009e70b29db6d6f6ac1ce8ea2bdd83f52
panda <panda@panda-desktop>
2017-10-15 01:47:48 -0300

    anda, casi exacto

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,65 @@
-
+#include "util.h"
 #include "lectura.cpp"
-#include <algorithm>  
+#include "pca.h"
+#include <algorithm>
 #include <vector>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 
-	
+
 	ifstream imagenesTrain;
 	imagenesTrain.open("../data/train.dat");
 	Matriz etiquetasTrain = Matriz(42000, 1);
 	Matriz imagenesTrainM = lecturaTrain(imagenesTrain, etiquetasTrain);
-	Matriz imagenesTrainMCopia=imagenesTrainM;
 	imagenesTrain.close();
-	
-    ifstream imagenesTest;
-	imagenesTest.open("../data/test.dat");
-	Matriz etiquetasTest = Matriz(28000, 1);
-	Matriz imagenesTestM = lecturaTest(imagenesTest, etiquetasTest);
-	
-	cout<<" TEST "<<endl; 
-	
-	mostrarImagen(imagenesTestM,1);
-	mostrarImagen(imagenesTestM,2);
-/*	mostrarImagen(imagenesTestM,3);
-	mostrarImagen(imagenesTestM,4);
-	mostrarImagen(imagenesTestM,5);
-	mostrarImagen(imagenesTestM,6);
-*/	
-		cout<<" TRAIN "<<endl; 
-		
-	mostrarImagen(imagenesTrainM,1);
-	mostrarImagen(imagenesTrainM,2);
-/*	mostrarImagen(imagenesTrainM,3);
-	mostrarImagen(imagenesTrainM,4);
-	mostrarImagen(imagenesTrainM,5);
-	mostrarImagen(imagenesTrainM,6);
-	*/
-	
-	Matriz imagenesTestMCopia=imagenesTestM;
-	imagenesTest.close();
+
+	//	ifstream imagenesTest;
+	//	imagenesTest.open("../data/test.dat");
+	//	Matriz etiquetasTest = Matriz(28000, 1);
+	//	Matriz imagenesTestM = lecturaTest(imagenesTest, etiquetasTest);
+	//	imagenesTest.close();
+
+	ifstream testfile;
+	testfile.open("../tests/testSimple1.in");
+	string ruta = "";
+	int vecinos = 0;
+	int alpha = 0;
+	int folds = 0;
+	testfile >> ruta >> vecinos >> alpha >> folds;
+	cout << "ruta: " << ruta << " - vecinos: " << vecinos << " - alpha: " << alpha << " - folds: " << folds << endl;
+	int flag = 0;
+	matrizReal trainmatrix;
+	vectorEntero trainlabels;
+
+	for (int x = 0; x < folds; x++) {
+		cout << "la de tu hermana en bolas" << endl;
+		trainmatrix.clear();
+		trainlabels.clear();
+		for (int y = 0; y < 42000; y++) {
+			testfile >> flag;
+			if (flag == 1) {
+				trainmatrix.push_back(imagenesTrainM.ObtenerFila(y + 1));
+				trainlabels.push_back(etiquetasTrain.ObtenerFila(y + 1)[0]);
+			}
+			//cout << flag << "-";
+		}
+		PCA metodo(trainmatrix, trainlabels, alpha, vecinos, 100, (10^-6));
+		for (int y = 0; y < metodo.autovalores.size(); y++) {
+			cout << metodo.autovalores[y] << endl;
+		}
+
+	}
+	testfile.close();
+	//PCA(matrizReal &imagenes, vectorEntero &labels, int alfa, int vecinos, int niter, double epsilon);
 
 
-	cout<<"knn"<<endl; 
-	
-	
-	int eti;
-	
-	Matriz Imagen_Test = Matriz(1,784);
-		    
-  for ( int i = 1  /* recorro 12*/
-  ;  i<12
-  ; ++i )
-  {
-       Imagen_Test = copiarFilaiesima(imagenesTestMCopia,i);
 
-       mostrarImagen(Imagen_Test,1);
-       
-       eti= knn(Imagen_Test,etiquetasTrain,imagenesTrainM, 10
-        );
 
-       
-    cout<<"El numero es: "<<eti<<endl;
-    
-	
-}
-return 0;
+
+	//leerParametros(argc, argv);
+
+
+
+	return 0;
 
 }
---
+++ src/util.h
@@ -0,0 +1,17 @@
+/*
+ * File:   util.h
+ * Author: panda
+ *
+ * Created on 14 de octubre de 2017, 21:00
+ */
+
+#ifndef UTIL_H
+#define UTIL_H
+
+void leerParametros(int argc, char const *argv[]) {
+
+}
+
+
+#endif /* UTIL_H */
+
